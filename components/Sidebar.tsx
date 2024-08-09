e111ebb54e103a1c83f87d80026f570c2166ca7d
UJJWAL <ujjwal8851114020@gmail.com>
2024-08-09 17:46:27 +0530

    5bje

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import React, { use } from 'react'
@@ -16,7 +17,8 @@ const Sidebar = () => {
                     <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
                         'bg-blue-1':isActive,
                     })}>
-                        {link.label}
+                        <Image src={link.imgUrl} alt={link.label} width={24} height={24}/>
+                        <p className='text-lg font-semibold max-lg:hidden'>{link.label}</p>
                     </Link>
                 )
             })}
