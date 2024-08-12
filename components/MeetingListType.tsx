942598bb3b4c07e33c51dd22dcd7c8a16c2fad40
UJJWAL <ujjwal8851114020@gmail.com>
2024-08-12 16:09:18 +0530

    4bje

--- components/MeetingListType.tsx
+++ components/MeetingListType.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 
 const MeetingListType = () => {
@@ -9,35 +10,47 @@ const MeetingListType = () => {
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+  const CreateMeeting=()=>{
+    
+  }
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
-        tile="New Meeting"
+        title="New Meeting"
         description="Start an instant Meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
         img="/icons/schedule.svg"
-        tile="Schedule Meeting"
+        title="Schedule Meeting"
         description="Plan your Meeting"
         handleClick={() => setMeetingState("isScheduleMeeting")}
         className="bg-blue-1"
       />
       <HomeCard
         img="/icons/recordings.svg"
-        tile="View Recordings"
+        title="View Recordings"
         description="Check out your recordings"
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-purple-1"
       />
       <HomeCard
         img="/icons/join-meeting.svg"
-        tile="Join Meeting"
+        title="Join Meeting"
         description="via invitation link"
         handleClick={() => setMeetingState('isJoiningMeeting')}
         className="bg-yellow-1"
+      />
+
+      <MeetingModal
+      isOpen={meetingState==='isInstantMeeting'}
+      onClose={()=>setMeetingState(undefined)}
+      title="Start an Instant meeting"
+      className="text-center"
+      buttonText="Start Meeting"
+      handleClick={CreateMeeting}
       />
     </section>
   );
